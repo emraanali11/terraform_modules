5f20820042df294e6faa224505974d79a396d3bc
iali2 <imran.ali@appdynamics.com>
2021-05-03 18:01:43 +0530

    addding ec2

--- ec2/main.tf
+++ ec2/main.tf
@@ -1,12 +1,16 @@
 # Find AMI for prod_cirdan
+terraform {
+  required_version = ">= 0.12.17"
+}
+
 provider "aws" {
-  region = "${var.region}"
+  region = "var.region"
 }
 
 resource "aws_instance" "instances" {
-  count                  = "${var.max_server_count}"
-  ami                    = "${var.ami_id}"
-  instance_type          = "${var.instance_type}"
-  vpc_security_group_ids = ["${var.vpc_security_group_ids}"]
-  subnet_id              = "${var.subnet}"
+  count                  = "var.max_server_count"
+  ami                    = "var.ami_id"
+  instance_type          = "var.instance_type"
+  vpc_security_group_ids = ["var.vpc_security_group_ids"]
+  subnet_id              = "var.subnet"
 }
